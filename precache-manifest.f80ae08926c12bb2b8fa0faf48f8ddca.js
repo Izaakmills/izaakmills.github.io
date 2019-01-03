eb6e4efca8605722fffdcea4769f7d44b21ad575
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2019-01-02 22:07:17 -0500

    Updates

---
+++ precache-manifest.f80ae08926c12bb2b8fa0faf48f8ddca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff51a5cfebeba23adf2755ab9b9fdb7d",
+    "url": "/react-portfolio/static/media/whitecappic.ff51a5cf.jpg"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "29ff07024a780567dfb6",
+    "url": "/react-portfolio/static/js/main.29ff0702.chunk.js"
+  },
+  {
+    "revision": "49b40192c83e63b5623f",
+    "url": "/react-portfolio/static/js/1.49b40192.chunk.js"
+  },
+  {
+    "revision": "29ff07024a780567dfb6",
+    "url": "/react-portfolio/static/css/main.cd23c077.chunk.css"
+  },
+  {
+    "revision": "0ff4eea1f0e21b382400e3daff539aae",
+    "url": "/react-portfolio/index.html"
+  }
+];
